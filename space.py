4b5fec9e583106a6d589ccc40710f8dd065ff4f6
ploychananya <ploy_youngmodulus@hotmail.com>
2017-09-28 20:19:47 +0700

    better code

--- space.py
+++ space.py
@@ -1,9 +1,22 @@
 import arcade
-from models import Ship
+from model import Ship
  
 SCREEN_WIDTH = 600
 SCREEN_HEIGHT = 600
+class ModelSprite(arcade.Sprite):
+    def __init__(self, *args, **kwargs):
+        self.model = kwargs.pop('model', None)
  
+        super().__init__(*args, **kwargs)
+ 
+    def sync_with_model(self):
+        if self.model:
+            self.set_position(self.model.x, self.model.y)
+ 
+    def draw(self):
+        self.sync_with_model()
+        super().draw()
+
 class SpaceGameWindow(arcade.Window):
     def __init__(self, width, height):
         super().__init__(width, height)
@@ -11,7 +24,8 @@ class SpaceGameWindow(arcade.Window):
         arcade.set_background_color(arcade.color.BLACK)
  
         self.ship = Ship(100,100)
-        self.ship_sprite = arcade.Sprite('images/ship.png')
+        #self.ship_sprite = arcade.Sprite('images/ship.png')
+        self.ship_sprite = ModelSprite('images/ship.png',model=self.ship)
  
  
     def on_draw(self):
@@ -22,7 +36,7 @@ class SpaceGameWindow(arcade.Window):
         ship = self.ship
  
         ship.update(delta)
-        self.ship_sprite.set_position(ship.x, ship.y)
+        #-------self.ship_sprite.set_position(ship.x, ship.y)
  
  
 if __name__ == '__main__':
